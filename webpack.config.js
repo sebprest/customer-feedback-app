0b041b6cc21f9b84c2458354276606a0d3277954
Sebastian Preston <38880204+sebprest@users.noreply.github.com>
2020-09-30 17:43:16 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/ts/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.html'],
+        alias: {
+            '^': path.resolve(__dirname, './src/ts')
+        }
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist/js'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({title: "Customer service page", template: "src/index.html"})
+    ]
+}
