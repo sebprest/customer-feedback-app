1b0683f2cd65d1c3c37ada780865e3ab636f44b6
Sebastian Preston <38880204+sebprest@users.noreply.github.com>
2020-10-01 17:34:10 +0100

    Use plural name in root reducer

--- src/ts/store/index.ts
+++ src/ts/store/index.ts
@@ -2,6 +2,6 @@ import { createStore, combineReducers } from "redux";
 
 import { review } from "^/app/review-form/reducers";
 
-const rootReducer = combineReducers({ review });
+const rootReducer = combineReducers({ reviews: review });
 
 export const store = createStore(rootReducer);
