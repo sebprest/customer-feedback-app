9b38788cf3f01c1a58d99bceee198d874ad325ff
Sebastian Preston <38880204+sebprest@users.noreply.github.com>
2020-10-03 20:06:05 +0100

    Add page contents to App component

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-const App: React.FunctionComponent = React.memo(() => <h1>Hello</h1>);
+import ReviewForm from "^/app/review-form";
+import ReviewList from "^/app/review-list";
+import RatingGraph from "^/app/rating-graph";
+
+const App: React.FunctionComponent = React.memo(() => (
+  <div className="container">
+    <div className="col col-4-md">
+      <ReviewForm />
+      <RatingGraph />
+    </div>
+    <div className="col col-8-md">
+      <ReviewList />
+    </div>
+  </div>
+));
 
 export default App;
